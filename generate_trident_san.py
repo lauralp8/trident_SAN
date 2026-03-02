c012e841aec7253a301d775df126412a6550fc5c
lauralp8 <lassol@netapp.com>
2026-03-02 13:22:42 +0100

    name spaces hardcodeados trident

--- generate_trident_san.py
+++ generate_trident_san.py
@@ -422,7 +422,7 @@ def create_backend_yaml(config: BackendConfig, secret_name: str) -> Dict[str, An
         'kind': 'TridentBackendConfig',
         'metadata': {
             'name': backend_name,
-            'namespace': 'trident_segundo'
+            'namespace': 'trident'
         },
         'spec': {
             'version': 1,
